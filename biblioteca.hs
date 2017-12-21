b9f3c0965e39fea91ae6098f5a5b3119bcbfbb71
rebecabbeltrao <rebeca.oliveira@ccc.ufcg.edu.br>
2017-12-21 17:39:33 -0300

    tentando atualizar

--- biblioteca.hs
+++ biblioteca.hs
@@ -1,4 +1,6 @@
-cadastra:: String -> String -> String -> IO()
+
+
+cadastra:: String -> String -> String -> String -> IO()
 cadastra nome autor editora alugado = do
  appendFile "titulos.txt" (nome++ ",")
  appendFile "autores.txt" (autor ++ ",")
@@ -21,6 +23,7 @@ listagemLivros = do
 	  let (editora:outraEditora) = listaLivros(c:cs) "" []
 	  let (alugados: maisAlugados) = listaLivros(d:ds) "" []
 	  imprime (nomeLivro:outrosLivros) (autor:outroAutores) (editora:outraEditora) (alugados: maisAlugados)
+	  
 
 imprime:: [String] -> [String] -> [String]-> [String] -> IO()
 imprime [""] [""] [""] [""] = putStrLn ("Listagem Encerrada")
@@ -37,12 +40,13 @@ listaLivros:: String -> String -> [String] -> [String]
 listaLivros [] nome [] = []
 listaLivros [] nome lista = lista ++ [nome]
 listaLivros (x:xs) i lista = do
-     if([x] == "*") then do 
+     if([x] == ",") then do 
          listaLivros xs (i ++ [x])lista
-     else if( [x] /= "*" && [x] /= "") then do
+     else if( [x] /= "," && [x] /= "") then do
          listaLivros xs "" (lista ++[i])
      else 
          lista ++ [i]
+
 
 -- "A" Para alugado e "N" Para não alugado 
 aluga :: String -> [String] -> String -> String
@@ -65,20 +69,30 @@ devolucao nome (head:titulos) (tail:alugados)
 
 -- atualiza (salva nomes na lista)
 atualiza :: String -> String -> [String] -> [String] -> [String]
-atualiza nome status [""] = []
+atualiza nome status [""] [""] = []
 atualiza nome status (x:titulos) (a:alugados) = do 
          if(nome /= x ) then do
              [x] ++  atualiza nome status titulos alugados
           else 
-              status ++ atualiza nome titulos alugados
+              status ++ atualiza nome status titulos alugados
 
+reescreve :: IO()
+reescreve = do 
+     writeFile "titulos.txt" ("")
+     writeFile "autores.txt" ("")
+     writeFile "editora.txt" ("")
+     
 -- alice A listadeT listA
-    
+
+
+
 main = do 
      print "Menu"
      putStrLn("1 - Cadastra Livro ")
      putStrLn("2 - Lista Livros ")
      putStrLn("3 - Aluga Livros ")
+     putStrLn("4 - Devolução") 
+     
      op <- getLine
      if (op == "1") then do
          putStrLn "CADASTRO DE LIVRO - Digite o nome do livro que deseja cadastrar:"
@@ -91,17 +105,17 @@ main = do
      else if( op == "2") then do
          listagemLivros
      else if( op=="3") then do
-         putStrLn "ALUGUEL DE LIVROS - Digite o nome do livro que deseja alugar:"
-         (x:xs) <- readFile ("titulos.txt")
-         aluguel <- readFile ("alugados.txt")
-         let listaTitulos <- listaLivros (x:xs) "" []
+        -- putStrLn "ALUGUEL DE LIVROS - Digite o nome do livro que deseja alugar:"
+         (x:xs) <- readFile "titulos.txt"
+         (w:ws) <- readFile "alugados.txt"
          nome <- getLine
-         let aa = aluga nome titulos listaTitulos
-         print aa
+         alugado <- getLine
+         let list = atualiza nome alugado (listaLivros (x:xs) "" []) (listaLivros (w:ws) "" [])
+         reescreve
+         print list
      else do
            a <- readFile "autores.txt"
            print a
            putStrLn "BUSCA DE LIVRO - Digite nome do livro a ser procurado:"
      main
-
 
