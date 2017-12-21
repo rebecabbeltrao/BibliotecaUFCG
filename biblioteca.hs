c8b1a971f4841c44efa7037662e4b36628b372df
rebecabbeltrao <rebeca.oliveira@ccc.ufcg.edu.br>
2017-12-21 17:01:32 -0300

    n ta compilando btw (ainda)

--- biblioteca.hs
+++ biblioteca.hs
@@ -1,20 +1,17 @@
 cadastra:: String -> String -> String -> IO()
 cadastra nome autor editora = do
- appendFile "titulos.txt" (nome++ "*")
- appendFile "autores.txt" (autor ++ "*")
- appendFile "editora.txt" (editora ++ "*")
+ appendFile "titulos.txt" (nome++ ",")
+ appendFile "autores.txt" (autor ++ ",")
+ appendFile "editora.txt" (editora ++ ",")
 
-
-remove :: String -> Bool
-remove livro = True
 
 listaLivros:: String -> String -> [String] -> [String]
 listaLivros [] nome [] = []
 listaLivros [] nome lista = lista ++ [nome]
 listaLivros (x:xs) i lista = do
-     if([x] == "*") then do 
+     if([x] == ",") then do 
          listaLivros xs (i ++ [x])lista
-     else if( [x] /= "*" && [x] /= "") then do
+     else if( [x] /= "," && [x] /= "") then do
          listaLivros xs "" (lista ++[i])
      else 
          lista ++ [i]
@@ -26,10 +23,30 @@ aluga nome [] [] = ""
 aluga nome [""] "" = ""
 aluga nome (head:titulos) (tail:alugados) 
              | (nome == head) = "A" ++ aluga nome titulos alugados 
-             | (nome /= head) = "N" ++ aluga nome titulos alugados
+             | (nome == "") = "" ++ aluga nome titulos alugados
+             | otherwise = [tail] ++ aluga nome titulos alugados
+
+-- devolução 
+devolucao :: String -> [String] -> String -> String
+devolucao nome [] [] = ""
+devolucao nome [""] "" = ""
+devolucao nome (head:titulos) (tail:alugados) 
+             | (nome == head) = "N" ++ aluga nome titulos alugados 
+             | (nome == "") = "" ++ aluga nome titulos alugados
              | otherwise = [tail] ++ aluga nome titulos alugados
 
 
+-- atualiza (salva nomes na lista)
+atualiza :: String -> String -> [String] -> [String] -> [String]
+atualiza nome status [""] = []
+atualiza nome status (x:titulos) (a:alugados) = do 
+         if(nome /= x ) then do
+             [x] ++  atualiza nome status titulos alugados
+          else 
+              status ++ atualiza nome titulos alugados
+
+-- alice A listadeT listA
+    
 main = do 
      print "Menu"
      op <- getLine
@@ -41,14 +58,18 @@ main = do
          cadastra nome autor editora
          
      else if( op == "2") then do
-         putStrLn "REMOÇÃO DE LIVRO - Digite o nome do livro que deseja remover:"
+         putStrLn "DEVOLUÇÃO DE LIVRO - Digite o nome do livro que deseja devolver:"
          nome <- getLine
          let aa = remove nome
          print aa
+         
      else if( op=="3") then do
-         putStrLn "ALUGUEL DE LIVROS - Digite o nome do livro que deseja remover:"
+         putStrLn "ALUGUEL DE LIVROS - Digite o nome do livro que deseja alugar:"
+         (x:xs) <- readFile ("titulos.txt")
+         aluguel <- readFile ("alugados.txt")
+         let listaTitulos <- listaLivros (x:xs) "" []
          nome <- getLine
-         let aa = remove nome
+         let aa = aluga nome titulos listaTitulos
          print aa
      else do
            a <- readFile "autores.txt"
