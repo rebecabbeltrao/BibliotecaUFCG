f1181985bdc2eff4eab461474b39b1b8639b7f8b
rebecabbeltrao <rebeca.oliveira@ccc.ufcg.edu.br>
2017-12-21 15:14:53 -0300

    mudança para biblioteca

--- biblioteca.hs
+++ biblioteca.hs
@@ -8,39 +8,30 @@ cadastra nome autor editora = do
 remove :: String -> Bool
 remove livro = True
 
-avalia :: String -> Int -> String
-avalia nome nota = "aa"
-
-
 main = do 
      print "PUTA"
      op <- getLine
      if (op == "1") then do
-         putStrLn "CADASTRO DE LIVRO - Digite o nome do filme que deseja cadastrar:"
+         putStrLn "CADASTRO DE LIVRO - Digite o nome do livro que deseja cadastrar:"
          nome <- getLine
          autor<- getLine
          editora <- getLine
          cadastra nome autor editora
          
      else if( op == "2") then do
-         putStrLn "REMOÇÃO DE LIVRO - Digite o nome do filme que deseja remover:"
+         putStrLn "REMOÇÃO DE LIVRO - Digite o nome do livro que deseja remover:"
          nome <- getLine
          let aa = remove nome
          print aa
      else if( op=="3") then do
-         putStrLn "AVALIAÇÃO DE LIVRO - Digite o nome e a nota do filme que deseja avaliar:"
-         nome <- getLine 
-         nota <- getLine
-         let a = avalia nome (read nota)
-         print a
-     else if( op== "4") then do
-         putStrLn "ALTERAÇÃO DE NOTA DE AVALIAÇÃO - Digite a nova nota de avaliação:" 
+         putStrLn "ALUGUEL DE LIVROS - Digite o nome do livro que deseja remover:"
          nome <- getLine
-         nota <- getLine 
-         let o = avalia nome (read nota)
-         print o          
+         let aa = remove nome
+         print aa
      else do
            a <- readFile "autores.txt"
            print a
-           putStrLn "BUSCA DE LIVRO - Digite nome do filme a ser procurado:"
+           putStrLn "BUSCA DE LIVRO - Digite nome do livro a ser procurado:"
      main
+
+
