50a707f7c99dd89510cbac66d61e10c7ece6bca3
rebecabbeltrao <rebeca.oliveira@ccc.ufcg.edu.br>
2017-12-22 14:50:30 -0300

    versão com tentativa de busca

--- biblioteca.hs
+++ biblioteca.hs
@@ -6,7 +6,7 @@ cadastra nome autor editora = do
  appendFile "autores.txt" (autor ++ ",")
  appendFile "editora.txt" (editora ++ ",")
  
- 
+
 listagemLivros = do
    nomeLivro <- readFile "titulos.txt"
    if(length nomeLivro == 0) then do
@@ -34,7 +34,8 @@ imprime (livro:outrosLivros) (autor:outroAutores) (editora:outraEditora) = do
       putStrLn ("Autor: " ++ autor)
       putStrLn ("Editora: " ++ editora)
       imprime outrosLivros outroAutores outraEditora
-     
+      
+-- funcao que gera uma lista de todos os elementos do arquivo
 listaLivros:: String -> String -> [String] -> [String]
 listaLivros [] nome [] = [nome]
 listaLivros [] nome lista = lista ++ [nome]
@@ -46,25 +47,35 @@ listaLivros (x:xs) i lista = do
      else
          lista ++ [i]
 ------------------------------------------------------------------
+-- escreve arquivo dos livros alugados
 escreveFileAlugados :: String -> IO()
 escreveFileAlugados alugado = do
   appendFile "alugados.txt" (alugado++ ",")
  
--- atualiza (salva nomes na lista)
+-- atualiza (salva nomes na lista de livros alugados)
 atualiza :: String -> [String] -> [String]
 atualiza nome [""] = []
 atualiza nome (x:alugados) = do
-         if(nome /= x ) then do
+         if(nome /= x) then do
              [x] ++  atualiza nome alugados
           else
              atualiza nome alugados
- 
+             
+-- verifica se o livro ja esta alugado
+verificaAlugado :: String -> [String] -> Bool
+verificaAlugado titulo [] = True
+verificaAlugado titulo [""] = True
+verificaAlugado titulo (x:xs)= if(x == titulo) then True
+                               else verificaAlugado titulo xs
+                               
+-- reescreve o arquivo de alugado em branco
 reescreve :: IO()
 reescreve = writeFile "alugados.txt" ("")
- 
+
+-- reescreve o arquivo com a nova lista de alugados 
 reescreveArquivo ::[String] -> IO()
-reescreveArquivo [] = putStrLn "Lista vazia"
-reescreveArquivo[""] = putStrLn "Lista vazia"
+reescreveArquivo [] = putStrLn ""
+reescreveArquivo[""] = putStrLn ""
 reescreveArquivo (x:xs) = do
       escreveFileAlugados x
       reescreveArquivo xs
@@ -72,7 +83,8 @@ reescreveArquivo (x:xs) = do
 -----------------------------
 main:: IO()
 main = do
-     print "Menu"
+     print "BIBLIOTECA UFCG"
+     putStrLn("Escolha uma das opções seguintes:")
      putStrLn("1 - Cadastra Livro ")
      putStrLn("2 - Lista Livros ")
      putStrLn("3 - Aluga Livros ")
@@ -84,18 +96,29 @@ main = do
          nome <- getLine
          autor<- getLine
          editora <- getLine
-         alugado <- getLine
          cadastra nome autor editora
          
      else if( op == "2") then do
-         listagemLivros
+         putStrLn "LISTAGEM DE LIVROS"
+         putStrLn "deseja listar todos os livros da biblioteca? S/N"
+         resposta <- getLine
+         if(resposta == "S") then do
+          listagemLivros
+         else do
+           putStrLn "Digite outra opção:"
      else if( op=="3") then do
+     
          putStrLn "ALUGUEL DE LIVROS - Digite o nome do livro que deseja alugar:"
-       
+         (x:xs) <- readFile "alugados.txt"
+         let livros = listaLivros (x:xs) "" []
          alugado <- getLine
-         escreveFileAlugados alugado
-         putStrLn "Livro alugado com sucesso!"
-     else if (op =="4") then do
+         
+         if(verificaAlugado alugado livros) then do 
+          putStrLn "Livro não disponível para aluguel"
+         else do
+           escreveFileAlugados alugado
+           putStrLn "Livro alugado com sucesso!"
+     else do
             devolvido <- getLine
             (x:xs) <- readFile "alugados.txt"
             let livros = listaLivros (x:xs) "" []
@@ -104,8 +127,5 @@ main = do
             a <- removeFile "titulos.txt"
             reescreve
             reescreveArquivo result
-     else do
-           a <- readFile "autores.txt"
-           print a
-           putStrLn "BUSCA DE LIVRO - Digite nome do livro a ser procurado:"
+    
      main
