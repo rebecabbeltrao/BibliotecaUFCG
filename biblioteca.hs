7df2151658b05099aeb7ff2b3f66b78c92a0445b
thiagocm1 <30806616+thiagocm1@users.noreply.github.com>
2017-12-22 17:32:25 -0200

    atualizacao lista de livros e devolucao
    90% pront

--- biblioteca.hs
+++ biblioteca.hs
@@ -6,7 +6,7 @@ cadastra nome autor editora = do
  appendFile "autores.txt" (autor ++ ",")
  appendFile "editora.txt" (editora ++ ",")
  
-
+ 
 listagemLivros = do
    nomeLivro <- readFile "titulos.txt"
    if(length nomeLivro == 0) then do
@@ -19,23 +19,19 @@ listagemLivros = do
       let (nomeLivro:outrosLivros) = listaLivros(a:as) "" []
       let (autor:outroAutores) = listaLivros (b:bs) "" []
       let (editora:outraEditora) = listaLivros(c:cs) "" []
-      print (nomeLivro:outrosLivros)
-      print (autor:outroAutores)
-      print (editora:outraEditora)
+  
  
       imprime (nomeLivro:outrosLivros) (autor:outroAutores) (editora:outraEditora)
      
  
 imprime:: [String] -> [String] -> [String] -> IO()
 imprime [""] [""] [""]  = putStrLn ("Listagem Encerrada")
-imprime [] [] []  = putStrLn ("Listagem Encerrada")
 imprime (livro:outrosLivros) (autor:outroAutores) (editora:outraEditora) = do
       putStrLn ("Nome Livro: " ++ livro)
       putStrLn ("Autor: " ++ autor)
       putStrLn ("Editora: " ++ editora)
       imprime outrosLivros outroAutores outraEditora
-      
--- funcao que gera uma lista de todos os elementos do arquivo
+     
 listaLivros:: String -> String -> [String] -> [String]
 listaLivros [] nome [] = [nome]
 listaLivros [] nome lista = lista ++ [nome]
@@ -47,35 +43,24 @@ listaLivros (x:xs) i lista = do
      else
          lista ++ [i]
 ------------------------------------------------------------------
--- escreve arquivo dos livros alugados
 escreveFileAlugados :: String -> IO()
 escreveFileAlugados alugado = do
   appendFile "alugados.txt" (alugado++ ",")
  
--- atualiza (salva nomes na lista de livros alugados)
+-- atualiza (salva nomes na lista)
 atualiza :: String -> [String] -> [String]
 atualiza nome [""] = []
 atualiza nome (x:alugados) = do
-         if(nome /= x) then do
+         if(nome /= x ) then do
              [x] ++  atualiza nome alugados
           else
              atualiza nome alugados
-             
--- verifica se o livro ja esta alugado
-verificaAlugado :: String -> [String] -> Bool
-verificaAlugado titulo [] = True
-verificaAlugado titulo [""] = True
-verificaAlugado titulo (x:xs)= if(x == titulo) then True
-                               else verificaAlugado titulo xs
-                               
--- reescreve o arquivo de alugado em branco
+ 
 reescreve :: IO()
 reescreve = writeFile "alugados.txt" ("")
-
--- reescreve o arquivo com a nova lista de alugados 
+ 
 reescreveArquivo ::[String] -> IO()
-reescreveArquivo [] = putStrLn ""
-reescreveArquivo[""] = putStrLn ""
+reescreveArquivo [] = putStrLn "Lista vazia"
 reescreveArquivo (x:xs) = do
       escreveFileAlugados x
       reescreveArquivo xs
@@ -83,8 +68,7 @@ reescreveArquivo (x:xs) = do
 -----------------------------
 main:: IO()
 main = do
-     print "BIBLIOTECA UFCG"
-     putStrLn("Escolha uma das opções seguintes:")
+     print "Menu"
      putStrLn("1 - Cadastra Livro ")
      putStrLn("2 - Lista Livros ")
      putStrLn("3 - Aluga Livros ")
@@ -93,39 +77,33 @@ main = do
      op <- getLine
      if (op == "1") then do
          putStrLn "CADASTRO DE LIVRO - Digite o nome do livro que deseja cadastrar:"
+         putStrLn "Informe o Titulo do Livro: "
          nome <- getLine
+         putStrLn "Informe o Autor do Livro: "
          autor<- getLine
+         putStrLn "Informe a Editora do Livro: "
          editora <- getLine
          cadastra nome autor editora
          
      else if( op == "2") then do
-         putStrLn "LISTAGEM DE LIVROS"
-         putStrLn "deseja listar todos os livros da biblioteca? S/N"
-         resposta <- getLine
-         if(resposta == "S") then do
-          listagemLivros
-         else do
-           putStrLn "Digite outra opção:"
-     else if( op=="3") then do
      
+         listagemLivros
+     else if( op=="3") then do
          putStrLn "ALUGUEL DE LIVROS - Digite o nome do livro que deseja alugar:"
+         alugado <- getLine
+         escreveFileAlugados alugado
+         putStrLn "Livro alugado com sucesso!"
+     else if (op =="4") then do
+         devolvido <- getLine
          (x:xs) <- readFile "alugados.txt"
          let livros = listaLivros (x:xs) "" []
-         alugado <- getLine
-         
-         if(verificaAlugado alugado livros) then do 
-          putStrLn "Livro não disponível para aluguel"
-         else do
-           escreveFileAlugados alugado
-           putStrLn "Livro alugado com sucesso!"
+         let result = atualiza devolvido livros
+         print result
+         a <- removeFile "alugados.txt"
+         reescreve
+         reescreveArquivo result
      else do
-            devolvido <- getLine
-            (x:xs) <- readFile "alugados.txt"
-            let livros = listaLivros (x:xs) "" []
-            let result = atualiza devolvido livros
-            print result
-            a <- removeFile "titulos.txt"
-            reescreve
-            reescreveArquivo result
-    
+           a <- readFile "autores.txt"
+           print a
+           putStrLn "BUSCA DE LIVRO - Digite nome do livro a ser procurado:"
      main
