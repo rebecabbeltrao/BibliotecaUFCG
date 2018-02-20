95ce0126d94a39e40267443b598b9979e07dcf6d
rebecabbeltrao <rebeca.oliveira@ccc.ufcg.edu.br>
2018-02-20 14:21:43 -0300

    maravilhaaaa nossas vidas \(◦'⌣'◦)/

--- Prolog/biblioteca.pl
+++ Prolog/biblioteca.pl
@@ -1,39 +1,36 @@
 :- initialization (main).
 
-cadastraTitulo([],Titulo,[Titulo]).
-cadastraTitulo([X|Y],Titulo,[X|Z]) :- cadastraTitulo(Y,Titulo,Z).
 
-cadastraAutor([],Autor,[Autor]).
-cadastraAutor([X|Y],Autor,[X|Z]) :- cadastraAutor(Y, Autor, Z).
+armazenaAlugados():- write("klb").
 
-
-cadastraEditora([],Editora,[Editora]).
-cadastraEditora([X|Y],Editora,[X|Z]) :- cadastraEditora(Y,Editora,Z).
-
-
-cadastraTitulo(Titulo) :- write("bababab").
-cadastraAutor(Autor) :- write("assdsds").
-cadastraEditora(Editora) :- write("wshdhdhwdj").
+listagemLivro([]):- main.
+listagemLivro([HEAD|TAIL]):- write("Titulo: "),nl,
+write(HEAD), nl,
+livro(HEAD,A,B), 
+write("Autor:"), nl,
+write(A), nl,
+write("Editora: "),nl,
+write(B),nl,
+listagemLivro(TAIL).
 
 runOption(1) :- 
  write("CADASTRO DE LIVRO - Digite o nome do livro que deseja cadastrar:"),nl,
  read(Titulo),
- cadastraTitulo(Titulo),
  write("Informe o Autor do Livro: "),nl,
  read(Autor), 
- cadastraAutor(Autor),
  write("Informe a Editora do Livro: "), nl,
  read(Editora),
- cadastraEditora(Editora).
- 
+ assertz(livro(Titulo, Autor, Editora)).
 
 runOption(2) :-
- write("LISTAGEM DE LIVROS"), nl.
+ write("LISTAGEM DE LIVROS"), nl,
+ findall(A,livro(A,B,C),Lista),
+ listagemLivro(Lista).
 
 runOption(3) :- 
 write("ALUGUEL DE LIVROS - Digite o nome do livro que deseja alugar: "), nl,
 read(LivroAlugar), 
-write( "Livro alugado com sucesso!"), nl.
+write("Livro alugado com sucesso!"), nl.
 
 runOption(4):-
 write("DEVOLUÇAO LIVROS - Digite o livro que deseja devolver: "), nl,
@@ -52,11 +49,4 @@ write("Por favor, informe a sua escolha: "), nl,
 read(Option),
 runOption(Option), main.
 
-
-/*
-runOption(Option) :- Option == 2, menuLista().
-runOption(Option) :- Option == 3, menuAluga().
-runOption(Option) :- Option == 4, menuDevolve().
-
-*/
 halt(0).
